fbc63a54e5e4698442003f1a2eba0ea35d8e87d9
Sergi <sergiusz.kozubek@gmail.com>
2020-10-31 22:35:27 +0100

    Added tiles to html, changes in start

--- app.js
+++ app.js
@@ -9,23 +9,27 @@
   };
 })(); */
 // Create Dino Objects
-const fetchDinos = async () => {
-  let url = './dino.json';
-  const response = await fetch(url)
-  const json = await response.json()
-  return json;
+// Fetch data from provided json file and return a promise
+async function fetchDinos() {
+  const url = './dino.json';
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.Dinos;
+  // expected output: ReferenceError: nonExistentFunction is not defined
+  // Note - error messages will vary depending on browser
 }
-//fetchDinos().then(data => createTiles(data));
+
+// fetchDinos().then(data => createTiles(data));
 // Create Human Object
 // Use IIFE to get human data from form
-//Creates human object
+// Creates human object
 function createHuman(name, feets, inches, weight, diet) {
   return {
-    name: name,
+    name,
     heightFeet: feets,
     heightInches: inches,
-    weight: weight,
-    diet: diet
+    weight,
+    diet,
   };
 }
 // Create Dino Compare Method 1
@@ -38,17 +42,17 @@ function createHuman(name, feets, inches, weight, diet) {
 // Add tiles to DOM
 // Remove form from screen
 // On button click, prepare and display infographic
-//Validate form, store data in the variables, remove the form and prepare the grid
-const validateForm = (name, feets, inches, weight) => {
+// Validate form, store data in the variables, remove the form and prepare the grid
+function validateForm(name, feets, inches, weight) {
   let valid = false;
   let message = '';
-  //validate form fields if not empty or invalid
+  // validate form fields if not empty or invalid
   if (name === '') {
     message = 'gimme your name';
   } else if (feets <= 0) {
     message = 'cmonn, gimme proper feets';
   } else if (inches <= 0) {
-    message = 'cmonn, gimme proper inches';
+    message = 'cmonn, gimme proper inchesss';
   } else if (weight <= 0) {
     message = 'cmonn, gimme proper weight';
   } else {
@@ -56,28 +60,36 @@ const validateForm = (name, feets, inches, weight) => {
   }
   return {
     verdict: valid,
-    message: message
-  }
+    message,
+  };
 }
-//Removes the form from the DOM
+// Removes the form from the DOM
 function removeForm() {
   const formUser = document.getElementById('dino-compare');
   formUser.remove();
 }
-//Here we start, executes on button click
+// Here we start, executes on button click
 function start() {
-  //get values form the form and set variables
+  // get values form the form and set variables
   const name = document.getElementById('name').value;
   const feets = document.getElementById('feet').value;
   const inches = document.getElementById('inches').value;
   const weight = document.getElementById('weight').value;
   const diet = document.getElementById('diet').value;
-  //if the form is valid, create human object
-  const isValid = validateForm(name, feets, inches, weight)
+  // if the form is valid, create human object
+  const isValid = validateForm(name, feets, inches, weight);
   if (isValid.verdict) {
-    let human = createHuman(name, feets, inches, weight, diet);
+    const human = createHuman(name, feets, inches, weight, diet);
     console.log(human);
-    //if the form is not valid, alert the message
-  } else
-    alert(isValid.message);
+    // if the form is not valid, alert the message
+  } else alert(isValid.message);
+
+  let dinos = [];
+  // fetchDinos().then(data => console.log(data));
+  (async function () {
+    // dinos will be the array of objects
+    dinos = await fetchDinos();
+
+    // console.log(dinos);
+  })();
 }
