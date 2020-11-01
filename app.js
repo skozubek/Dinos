0eeb7dcae5449504ea4786a4aca2fe8da9b278eb
Sergi <sergiusz.kozubek@gmail.com>
2020-11-01 22:42:35 +0100

    Added comparsion logic in create dino tiles module to set the fact

--- app.js
+++ app.js
@@ -1,44 +1,85 @@
-// Create Dino Constructor
-/* let dino = (function (dino, picUrl) {
-  return {
-    dinos: dino.species,
-    weight: dino.weight,
-    height: dino.height,
-    diet: dino.diet,
-    pic: picUrl
-  };
-})(); */
-// Create Dino Objects
-// Fetch data from provided json file and return a promise
-async function fetchDinos() {
-  const url = './dino.jsongit';
-  const response = await fetch(url);
-  const json = await response.json();
-  return json.Dinos;
-  // expected output: ReferenceError: nonExistentFunction is not defined
-  // Note - error messages will vary depending on browser
-}
+// Create Dino Tiles objects using revaling module pattern
+const createDinoTile = (function () {
+  function setFact(dino, human) {
+    // Weight Compare Method
+    function compareWeight() {
+      return `Dino weight was ${dino.weight} lbs, your weight is only ${human.weight} lbs`;
+    }
+    // Height Compare Method
+    function compareHeight() {
+      return `Dino was ${dino.height} heigh, you are only ${human.feets} feets and ${human.inches} inches tall`;
+    }
+    // Diet Compare Method
+    function compareDiet() {
+      return `Dino's diet was ${dino.diet}. You are ${human.diet}`;
+    }
+    // variable to store the fact text.
+    let fact = ``;
+    // If it is Pigeon, always the fact stored in json file will be displayed.
+    // Otherwise, random info from the json file will be displayed.
+    if (dino.species === 'Pigeon') {
+      fact = dino.fact;
+    } else {
+      // get random number from 0-5
+      // if we get 0 - the fact will be weight
+      // if we get 1 - the fact will be height
+      // if we get 2 - the fact will be diet
+      // if we get 3 - the fact will be fact
+      // if we get 4 - the fact will be where
+      // if we get 5 - the fact will be when
+      const number =
+        Math.floor(Math.random() * (Math.floor(5) - Math.ceil(0))) +
+        Math.ceil(0);
 
-// fetchDinos().then(data => createTiles(data));
-// Create Human Object
-// Use IIFE to get human data from form
+      switch (number) {
+        case 0:
+          fact = compareWeight();
+          break;
+        case 1:
+          fact = compareHeight();
+          break;
+        case 2:
+          fact = compareDiet();
+          break;
+        case 3:
+          fact = dino.fact;
+          break;
+        case 4:
+          fact = `Dino lived in: ${dino.where}`;
+          break;
+        case 5:
+          fact = `Dino lived in ${dino.when}`;
+          break;
+        default:
+          console.log(`sth went wrong`);
+      }
+    }
+    return fact;
+  }
+  // creates data to be displayed on tiles
+  function createTile(dino, human) {
+    const fact = setFact(dino, human);
+    const { species, url } = dino;
+    return {
+      fact,
+      species,
+      url,
+    };
+  }
+  return {
+    create: createTile,
+  };
+})();
 // Creates human object
 function createHuman(name, feets, inches, weight, diet) {
   return {
     name,
-    heightFeet: feets,
-    heightInches: inches,
+    feets,
+    inches,
     weight,
     diet,
   };
 }
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-// Generate Tiles for each Dino in Array
 // Add tiles to DOM
 // Remove form from screen
 // On button click, prepare and display infographic
@@ -68,6 +109,13 @@ function removeForm() {
   const formUser = document.getElementById('dino-compare');
   formUser.remove();
 }
+// Fetch data from provided json file and return a promise
+async function fetchDinos() {
+  const url = './dino.json';
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.Dinos;
+}
 // Here we start, executes on button click
 function start() {
   // get values form the form and set variables
@@ -76,20 +124,27 @@ function start() {
   const inches = document.getElementById('inches').value;
   const weight = document.getElementById('weight').value;
   const diet = document.getElementById('diet').value;
-  // if the form is valid, create human object
+  // check if the form is valid
   const isValid = validateForm(name, feets, inches, weight);
+  // if it is proceed to create objects
   if (isValid.verdict) {
+    // create human object
     const human = createHuman(name, feets, inches, weight, diet);
-    console.log(human);
+    // create dinos objects
+    // getting json into array
+    // create tiles based on the fetched data
+    (async function () {
+      // dinos will be the array of objects
+      const data = await fetchDinos();
+      // add img url to every object
+      const dinos = data.map((item) => ({
+        ...item,
+        url: `./images/${item.species}.png`,
+      }));
+      // create dino tiles based on dino and human for comparsion
+      const tiles = dinos.map((dino) => createDinoTile.create(dino, human));
+      console.log(tiles);
+    })();
     // if the form is not valid, alert the message
   } else alert(isValid.message);
-
-  let dinos = [];
-  // fetchDinos().then(data => console.log(data));
-  (async function () {
-    // dinos will be the array of objects
-    dinos = await fetchDinos();
-
-    // console.log(dinos);
-  })();
 }
